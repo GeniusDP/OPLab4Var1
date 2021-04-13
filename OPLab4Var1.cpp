56c55b41ae70ebfc2fbdf9f50c3eecee2db10ad1
Vladyslav2020 <vmskl2020@gmail.com>
2021-04-13 20:42:33 +0300

    Добавил файлы для структур BMP

--- OPLab4Var1.cpp
+++ OPLab4Var1.cpp
@@ -1,39 +1,9 @@
 ﻿#include <iostream>
 #include <vector>
 #include <fstream>
-
+#include "bmpStruct.h"
+#include "pixelStruct.h"
 using namespace std;
-
-#pragma pack(push, 1)
-struct BMP {
-    int8_t id0;
-    int8_t id1;// Завжди дві літери 'B' і 'M'
-    int32_t filesize;        // Розмір файла в байтах!!!!!!!!!!!!!!!!!!!!!!
-    int32_t reserved;     // 0, 0
-    int32_t headersize;      // 54L для 24-бітних зображень
-    int32_t infoSize;        // 40L для 24-бітних зображень
-    int32_t width;           // ширина зображення в пікселях!!!!!!!!!!!!!!!!!!!!!!
-    int32_t depth;           // висота зображення в пікселях!!!!!!!!!!!!!!!!!!!!!!)))
-    int16_t biPlanes;        // 1 (для 24-бітних зображень)
-    int16_t bits;            // 24 (для 24-бітних зображень)
-    int32_t biCompression;   // 0L
-    int32_t biSizeImage;     // Можна поставити в 0L для зображень без компрессії (наш варіант)
-    int32_t biXPelsPerMeter; // Рекомендована кількість пікселів на метр, можна 0L
-    int32_t biYPelsPerMeter; // Те саме, по висоті
-    int32_t biClrUsed;       // Для індексованих зображень, можна поставити 0L
-    int32_t biClrImportant;  // Те саме
-};
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-struct PIXEL {
-    char B;
-    char G;
-    char R;
-};
-#pragma pack(pop)
-
-
 
 int main()
 {
