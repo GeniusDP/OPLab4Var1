d8b823e027fc6ec4ac7fa0a53a4f2d5922f303aa
GeniusDP <zaranikbz@gmail.com>
2021-04-13 22:12:20 +0300

    Внесены изменения main с целью использования сразу двух алгоритмов для сравнения качества выходного изборажения.

--- OPLab4Var1.cpp
+++ OPLab4Var1.cpp
@@ -13,17 +13,22 @@ using namespace std;
 
 
 int main(int argc, char* argv[]) {
-    double times = atof(argv[3]);
+    double times = atof(argv[4]);
     Picture existingImage;
-    Picture enlargedImage;
+    Picture enlargedImageBogdan;
+    Picture enlargedImageVlad;
+
     Reader newReader((string)argv[1]);//"bmp.bmp"
-    Printer newPrinter((string)argv[2]);//"output.bmp"
+    Printer newPrinterBogdan((string)argv[2]);//"output.bmp"
+    Printer newPrinterVlad((string)argv[3]);
 
     newReader._read(existingImage);
 
-    enlargedImage = existingImage.interpolate(times);
-
-    newPrinter._print(enlargedImage);
+    enlargedImageVlad = existingImage.enlargerVlad(times);
+    enlargedImageBogdan = existingImage.interpolate(times);
+    
+    newPrinterBogdan._print(enlargedImageBogdan);
+    newPrinterVlad._print(enlargedImageVlad);
     cerr << "Done!";
     return 0;
 }
